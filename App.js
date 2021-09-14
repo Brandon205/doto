c538ac4037697a47148909e1a7a9e72a734ea11e
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2021-09-14 08:29:42 -0700

    Starting navigation

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Soon from './components/Soon';
+import Later from './components/Later';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Soon">
+        <Stack.Screen name="Soon" component={Soon} />
+        <Stack.Screen name="Later" component={Later} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
--- components/Soon.js
+++ components/Soon.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 export default function Soon() {
     return (
         <View>
-            <Text>Soon</Text>
+            <Text>Soon component</Text>
         </View>
     )
 }
