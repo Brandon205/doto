aa622bb251006badf240b69afad45d419397c7a1
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2021-09-22 12:00:14 -0700

    Testing/getting Async Storage to work

--- components/Soon.js
+++ components/Soon.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const fakeData = [
     {
@@ -11,14 +12,19 @@ const fakeData = [
     },
     {
         id: 2,
-        title: 'Call for guitar',
-        desc: 'See if they can',
+        title: 'Call for Guitar',
+        desc: 'See if they can fix it',
         createdOn: '9/21/21',
         due: '9/21/21'
     }
 ]
 
 export default function Soon() {
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+        getData()
+    }, [data])
 
     const renderItem = ({ item }) => {
         return (
@@ -30,10 +36,33 @@ export default function Soon() {
         )
     }
 
+    const addData = async () => {
+        let tempJson = JSON.stringify(fakeData)
+        try {
+            await AsyncStorage.setItem('testingData', tempJson)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const getData = async () => {
+        try {
+            let jsonValue = await AsyncStorage.getItem('testingData')
+            // console.log(jsonValue)
+            if (jsonValue !== null) {
+                setData(JSON.parse(jsonValue))
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <View style={styles.container}>
             <Text>Soon Component</Text>
-            <FlatList data={fakeData} renderItem={renderItem} keyExtractor={(item) => item.id} />
+            <FlatList data={data} renderItem={renderItem} keyExtractor={(item) => item.id} />
+            <Button title="Add Data" onPress={addData}></Button>
+            <Button title="Get Data" onPress={getData}></Button>
         </View>
     )
 }
